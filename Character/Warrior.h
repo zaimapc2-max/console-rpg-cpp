e616b0a2868aee706ca9254db68ab6a987343cc4
zaimapc2-max <zaimapc2@gmail.com>
2026-06-16 10:15:36 +0500

    add Warrior.h - Warrior class declaration

---
+++ Character/Warrior.h
@@ -0,0 +1,17 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+#include "Character.h"
+using namespace std;
+
+class Warrior : public Character{
+    protected:
+            int rage;
+    public:
+        Warrior(string na);
+        void attack() override;
+        void displayStats() override;
+        void takeDamage(int dmg) override;
+        void shieldBash(Character& target);
+};
+
+#endif
